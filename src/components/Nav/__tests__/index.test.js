c7d466bce56c30bf73e949d850a1cb78acabe216
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-09-28 07:05:28 -0500

    Update Nav tests to include props

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,36 +1,66 @@
-import React from 'react';
-import { render, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect'; 
-import Nav from '..';
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Nav from "..";
+
+const categories = [
+  { name: "portraits", description: "Portraits of people in my life" },
+];
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
 
 afterEach(cleanup);
 
-describe('Nav component', () => {
+describe("Nav component", () => {
   //baseline test
-  it('renders', () => {
-    render(<Nav/>);
+  it("renders", () => {
+    render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+      />
+    );
   });
   //snapshot test
-  it('matches snapshot', () => {
-    const { asFragment } = render(<Nav/>);
+  it("matches snapshot", () => {
+    const { asFragment } = render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+      />
+    );
     // assert value comparison
     expect(asFragment()).toMatchSnapshot();
-  })
-})
-
-describe('emoji is visible', () => {
-  it('inserts emoji into the h2', () => {
-    // Arrange
-    const { getByLabelText } = render(<Nav/>);
-    // Assert
-    expect(getByLabelText('camera')).toHaveTextContent('📸');
-  })
-})
-
-describe('links are visible', () => {
-  it('inserts text into the links', () => {
-    const { getByTestId } = render(<Nav />);
-    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-    expect(getByTestId('about')).toHaveTextContent('About me');
   });
-})
+});
+
+describe("emoji is visible", () => {
+  it("inserts emoji into the h2", () => {
+    // Arrange
+    const { getByLabelText } = render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+      />
+    );
+    // Assert
+    expect(getByLabelText("camera")).toHaveTextContent("📸");
+  });
+});
+
+describe("links are visible", () => {
+  it("inserts text into the links", () => {
+    const { getByTestId } = render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+      />
+    );
+    expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
+    expect(getByTestId("about")).toHaveTextContent("About me");
+  });
+});
