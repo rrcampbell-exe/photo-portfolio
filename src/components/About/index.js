c87e944a47659090e80a8b8c529897358dc43e36
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-09-30 10:49:17 -0500

    Update about text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,9 +4,9 @@ const About = () => {
   return (
     <section className="my-5">
       <h1 id="about">About</h1>
-      <p>Cats. I've got 'em, and the internet loves photos of 'em—cats in general, I mean, but also my cats. Well, <i>our</i> cats. They're not just mine. My wife would tell you Rhaegar is her cat and Hashtag is my cat, but she's mostly just teasing. Mostly.</p>
-      <p>Honestly, that's probably what the cats would tell you, too. They've got favorites and so do we. Let's not pretend otherwise.</p>
-      <p>Anyway, aside from showing off photos of *ahem* <i>our</i> cats, this site is for showing off <i>my</i> skills in the realm of React, test-driven development, and developing single-page applications.</p>
+      <p>Cats. I've got 'em, and the internet loves 'em—cats in general, I mean, but also my cats. Well, <i>our</i> cats. They're not just mine. My wife would tell you Rhaegar is her cat and Hashtag is my cat, but she's mostly just teasing. Mostly.</p>
+      <p>Though if we're honest, that's probably what the cats would tell you, too. They've got favorites and so do we. Let's not pretend otherwise.</p>
+      <p>Anyway, aside from showing off photos of cats, this site is for showing off <i>my</i> skills in the realm of ⚛️ React, 🧪 test-driven development, and 📄 single-page application development.</p>
       <p>So take a look around using <a href="#navbar">the navbar</a> up top, or jump straight to <a href="https://github.com/rrcampbell-exe/photo-portfolio" target="_blank">the GitHub repo</a> for this app. It's not like the cats will bite you either way. Probably.</p>
     </section>
   );
