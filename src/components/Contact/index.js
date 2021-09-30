07d467d94c809671490013834770f86baba3ad24
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-09-30 10:56:47 -0500

    Update contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,80 +1,19 @@
-import React, { useState } from "react";
-import { validateEmail, capitalizeFirstLetter } from "../../utils/helpers";
+import React from "react";
 
-function ContactForm() {
-  const [formState, setFormState] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-  const { name, email, message } = formState;
-
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const handleChange = (e) => {
-    if (e.target.name === "email") {
-      const isValid = validateEmail(e.target.value);
-      if (!isValid) {
-        setErrorMessage("Your email is invalid.");
-      } else {
-        setErrorMessage("");
-      }
-    } else {
-      if (!e.target.value.length) {
-        setErrorMessage(`${capitalizeFirstLetter(e.target.name)} is required.`);
-      } else {
-        setErrorMessage("");
-      }
-    }
-  };
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-    }
-  }
+function ContactPage() {
 
   return (
     <section>
-      <h1>Contact me</h1>
-      <form id="contact-form" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            name="name"
-            defaultValue={name}
-            onBlur={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email address:</label>
-          <input
-            type="email"
-            name="email"
-            defaultValue={email}
-            onBlur={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea
-            name="message"
-            rows="5"
-            defaultValue={message}
-            onBlur={handleChange}
-          />
-        </div>
-        {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
-          </div>
-        )}
-        <button type="submit">Submit</button>
-      </form>
+      <h1>Contact</h1>
+      <p>Did you know I'm on the internet? Oh, uh, I suppose you did.</p> 
+      <p>But did you know I'm <i>these places</i> on the internet? Ha! I bet not.</p>
+      <ul>
+        <a href="https://rrcampbell-exe.github.io/" rel="noreferrer"><li>Dev Portfolio</li></a>
+        <a href="https://github.com/rrcampbell-exe" rel="noreferrer"><li>GitHub</li></a>
+        <a href="https://www.linkedin.com/in/ryan-campbell-aa33169/" rel="noreferrer"><li>LinkedIn</li></a>
+      </ul>
     </section>
   );
 }
 
-export default ContactForm;
+export default ContactPage;
